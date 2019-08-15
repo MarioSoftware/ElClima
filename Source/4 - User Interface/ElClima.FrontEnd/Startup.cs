27d88776f33cbf4a5554ba5cd2990945c761e652
mpedernera <mpedernera@HYAMPEDERNERANB>
2019-08-15 16:06:45 -0300

    Agrego Areas estructura y configuracion inicial, y agrego controller y carpeta views para vista Base

--- Source/4 - User Interface/ElClima.FrontEnd/Startup.cs
+++ Source/4 - User Interface/ElClima.FrontEnd/Startup.cs
@@ -15,6 +15,7 @@ namespace ElClima.FrontEnd
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,15 @@ namespace ElClima.FrontEnd
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                // Base page
+                routes.MapRoute("default", "{controller=Base}/{action=Index}");
+                
+                routes.MapRoute("areas", "{area:exists}/{controller=Home}/{action=List}/{id?}");
             });
+
         }
     }
 }
