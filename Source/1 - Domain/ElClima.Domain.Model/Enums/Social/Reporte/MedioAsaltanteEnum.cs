96f32ed98ee054150ec5a2b1d73548e81963c7dc
mpedernera <mpedernera@HYAMPEDERNERANB>
2019-08-18 21:38:19 -0300

    Subo carpeta Enums y Models en Domain.Models

---
+++ Source/1 - Domain/ElClima.Domain.Model/Enums/Social/Reporte/MedioAsaltanteEnum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElClima.Domain.Model.Enums.Social.Reporte
+{
+    public enum MedioAsaltanteEnum
+    {
+        [Description("Motocicleta")]
+        Motocicleta = 1,
+
+        [Description("Automovil")]
+        Automovil = 2,
+
+        [Description("A pie")]
+        APie = 3,
+
+        [Description("No definido")]
+        NoDefinido = 4
+    }
+
+    public static partial class Extensions
+    {
+        public static string GetDescription(this MedioAsaltanteEnum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+
+            return Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute
+                ? attribute.Description
+                : value.ToString();
+        }
+    }
+}
