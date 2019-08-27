c10af53d0254c843e26145010f04cab52f0fb9ad
mpedernera <mpedernera@HYAMPEDERNERANB>
2019-08-27 12:49:30 -0300

    Avanso con la creacion de modelos para Entidad

---
+++ Source/1 - Domain/ElClima.Domain.Model/Models/Social/Entidades/Producto.cs
@@ -0,0 +1,20 @@
+﻿using ElClima.Domain.Core.Entities;
+using ElClima.Domain.Model.Models.Posicionamiento;
+using ElClima.Domain.Model.Models.Social.Sujetos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElClima.Domain.Model.Models.Social.Entidades
+{
+    public class Producto : BaseEntity
+    {
+        public Servicio servicio { get; set; }
+
+        public string descripcion { get; set; }
+
+        public decimal precio { get; set; }
+
+        public bool disponible { get; set; } 
+    }
+}
---
+++ Source/1 - Domain/ElClima.Domain.Model/Models/Social/Entidades/ProductoImagen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElClima.Domain.Model.Models.Social.Entidades
+{
+    class ProductoImagen
+    {
+        public Producto producto { get; set; }
+
+        public string descripcion { get; set; }
+
+        public DateTime fechaHoraSubida { get; set; }
+    }
+}
