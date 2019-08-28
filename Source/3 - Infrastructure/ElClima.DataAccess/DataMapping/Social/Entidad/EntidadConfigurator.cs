83bbeb772e238ceb31cd0777ad003771fd476ab8
mpedernera <mpedernera@HYAMPEDERNERANB>
2019-08-28 19:27:18 -0300

    Primer clase configurator para formar tabla Entidad en base de datos.

---
+++ Source/3 - Infrastructure/ElClima.DataAccess/DataMapping/Social/Entidad/EntidadConfigurator.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ElClima.DataAccess.DataMapping.Social.Entidad
+{
+    internal static class EntidadConfigurator
+    {
+        public static void Configure(ModelBuilder modelBuilder)
+        {
+            //Entity Table
+            modelBuilder.Entity<ElClima.Domain.Model.Models.Social.Entidades.Entidad>(e =>
+            {
+                e.ToTable("Entidad", "Entidades");
+
+                e.Property<int>("id")
+                .IsRequired()
+                .UseSqlServerIdentityColumn()
+                .ValueGeneratedOnAdd();
+
+                e.Property<string>("descripcion")
+                   .IsRequired()
+                   .HasColumnType("varchar(50)");
+            });
+            // Creamos la ForeingKey Cliente-Calificacion
+            modelBuilder.Entity<ElClima.Domain.Model.Models.Social.Entidades.Entidad>()
+                .Property<int>("TipoEntidadId");
+            modelBuilder.Entity<ElClima.Domain.Model.Models.Social.Entidades.Entidad>()
+                .HasOne(c => c.tipoEntidad);
+        }
+    }
+}
