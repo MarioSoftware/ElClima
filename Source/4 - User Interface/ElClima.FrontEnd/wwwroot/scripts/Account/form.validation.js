5ca00be8f9995c75896a3a27c4b2568cc24b0764
mpedernera <mpedernera@HYAMPEDERNERANB>
2019-10-31 12:04:56 -0300

    Agrego libreria jquery validation y agrego validator script para accountForm

---
+++ Source/4 - User Interface/ElClima.FrontEnd/wwwroot/scripts/Account/form.validation.js
@@ -0,0 +1,50 @@
+﻿var validator = $("#accountForm").validate({
+
+    rules: {
+        dni: {
+            required: true,
+            maxlength:8
+        },
+        contrasenia: {
+            minlength: 8,
+            maxlength: 16,
+            required:true
+        },
+        contraseniaRepetir: {
+            minlength: 8,
+            maxlength: 16,
+            required: true,
+            equalTo:"#contrasenia"
+        }
+    },
+
+    messages: {
+        dni: {
+            required: "Ingresa el DNI",
+            maxlength:"Debe contener 8 digitos"
+        },
+        contrasenia: {
+            required: "Ingresa una contraseña", 
+            minlength: "La contraseña debe ser mayor a 8 digitos",
+            maxlength: "La contraseña debe ser menor a 16 digitos"
+        },
+        contraseniaRepetir: {
+            required: "Ingresa una contraseña",            
+            minlength: "La contraseña debe ser mayor a 8 digitos",
+            maxlength: "La contraseña debe ser menor a 16 digitos",
+            equalTo: "Las contraseñas no coinciden"
+        }
+    },
+
+    ignore: "",
+
+    submitHandler: function (form) {
+
+        form.submit();
+    },
+
+    invalidHandler: function () {
+
+    }
+
+});
