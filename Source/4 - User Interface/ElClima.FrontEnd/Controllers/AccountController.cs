75b638a683c86c5dcfe91d8d8439cb2f7d61ba67
mpedernera <mpedernera@HYAMPEDERNERANB>
2019-11-05 11:30:47 -0300

    Creo vista Login y su Action

--- Source/4 - User Interface/ElClima.FrontEnd/Controllers/AccountController.cs
+++ Source/4 - User Interface/ElClima.FrontEnd/Controllers/AccountController.cs
@@ -2,18 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElClima.FrontEnd.Controllers
 {
     public class AccountController : Controller
     {
-         
+        [AllowAnonymous]
         public IActionResult Add()
         {
             ViewBag.Id = -1;
             return View("AccountForm");
         } 
+
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
     }
 }
